bbf2e678585ee28c275c5db30f6fd02cbc9f3c35
karlita101 <71699120+karlita101@users.noreply.github.com>
2020-10-20 13:14:34 +0200

    Revision 1 of RGB Camera calibration
    Camera calibration.
    
    Begins video stream with while loop.
    press ENTER to beging capturing images at any time.
    
    Press esc to exit video stream at any time.
    
    Print the number of images used in the calibration.
    
    Save coefficients

--- Cameralcalibration.py
+++ Cameralcalibration.py
@@ -41,7 +41,7 @@ def drawchessboard(square_size, width, height,dirpath, prefix, image_format):
     imgpoints = [] # 2d points in image plane.
 
     stat_images = glob.glob(dirpath+'/' + prefix + '*.' + image_format)
-    
+    ##print(len(stat_images))
     #Start counter for images used
     found_img=0
     for fname in stat_images:
@@ -49,15 +49,18 @@ def drawchessboard(square_size, width, height,dirpath, prefix, image_format):
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
         # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (width,height),None)
+        ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
+        print(ret)
 
         # If found, add object points, image points (after refining them)
         if ret == True: #ret or retval is a variable used to assign the status of the last executed command to the retval variable
             objpoints.append(objp)
+            
             corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
             imgpoints.append(corners2)
+            
             # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (width,height), corners2,ret)
+            img = cv2.drawChessboardCorners(img, (width, height), corners2, ret)
             found_img+=1 # count +1 when corneres are found in each frame
             
             cv2.imshow ('Image', img)
@@ -66,51 +69,55 @@ def drawchessboard(square_size, width, height,dirpath, prefix, image_format):
     print("Number of images used for calibration: ", found_img)
     cv2.destroyAllWindows()
     #Calibration
+    
     # Get camera matrix, distortion coefficients, rotation and translation vector.  
     rms, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
     
     #undistort
-    undistort(stat_imgages,mtx,dist)
-    return [rms, mtx, dist, rvecs, tvecs]
+    ####undistort(stat_images,mtx,dist)
+    return [rms, mtx, dist, rvecs, tvecs, stat_images]
 
     #cv2.destroyAllWindows()
 
-def save_coefficients(mtx, dist, path):
+def save_coefficients(mtx, dist, dirpath):
     """ Save the camera matrix and the distortion coefficients to given path/file. """
-    cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
+    cv_file = cv2.FileStorage(dirpath+'/'+'calibration.txt', cv2.FILE_STORAGE_WRITE)
     cv_file.write("K", mtx)
     cv_file.write("D", dist)
     # note you *release* you don't close() a FileStorage object
     cv_file.release()
 
-def undistort(stat_imgages,mtx,dist):
-    count=1
-    for fname in images:
-        img = cv2.imread(fname)
-        h,w=image.shape[:2]
-        newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-        dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+def undistort(stat_images,mtx,dist):
+    #count=1
+    #for fname in stat_images:
+        #img = cv2.imread(fname)
+        #h,w=stat_images.shape[:2]
+        #newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+        #dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
         
         # crop the image
-        x,y,w,h = roi
-        dst = dst[y:y+h, x:x+w]
-        countstr=str(countstr)
-        cv2.imwrite('calibresult'+countstr+'.png',dst)
-        count+=1
+        #x,y,w,h = roi
+        #dst = dst[y:y+h, x:x+w]
+        #countstr=str(countstr)
+        #cv2.imwrite('calibresult'+countstr+'.png',dst)
+        #count+=1
 
 # Configure depth and color streams
+
 pipeline = rs.pipeline()
 config = rs.config()
 fps=30
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, fps)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, fps)
 
+count=0
 # Start streaming
 pipeline.start(config)
 
 try:
     
     while True:
+        
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
@@ -133,40 +140,16 @@ try:
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
         cv2.waitKey(1)
-        
-        #Capture image
-        count=0
-        #cv2.waitKey(1)
-        #print("Press 'p'  key to begin capturing images ")
-        
-        #status=""
-        #capture=input("To capture imagepress p"
-        #if keyboard.is_pressed('p'):
-        
-        #if capture=="p":
-        #while capture==p": 
-        #while keyboard.is_pressed('p')
-        
-        
-
-        print("Would  you like to  begin capturing photos? Enter  yes to start")
-        answer = input()
-
-        while answer=='yes':
-            print("to capture image press y")    
-            prompt=input()
-            if prompt=='y':
-                count+=1
-                cap_img=cv2.imwrite('Image'+str(count)+'.png', images)
-                print("Image has been captured. Would you like to proceed?")
-                proceed=input()
-                if proceed=='n':
-                    break
-            else:
-                break
-        print('Count images',  count)
-
-        
+   
+        #Enter to begin capturing images
+        if keyboard.is_pressed('Enter'):
+            count+=1
+            cap_img=cv2.imwrite('Image'+str(count)+'.png', images)
+            print("Image has been captured.")
+            #proceed=input()
+            continue 
+              
+        #ESC to exit video stream at any time
         if keyboard.is_pressed('esc'):
             break
     
@@ -177,6 +160,7 @@ finally:
     # Stop streaming
     pipeline.stop()
     print("Pipeline has been stopped")
+    print('Count images',  count)
     
 
 ######################################################################################
@@ -185,11 +169,12 @@ finally:
 
 #Get cameracalibration   
 square_size =0.024
-width=9
-height=7
+width=8
+height=6
 image_format='png'
 prefix='Image'
-rms, mtx, dist, rvecs, tvecs= drawchessboard(square_size, width, height,dirpath, prefix, image_format)
+rms, mtx, dist, rvecs, tvecs, stat_images= drawchessboard(square_size, width, height,dirpath, prefix, image_format)
 
-# Save coefficients
+#Save coefficients
 #save_coefficients(mtx, dist, dirthpath):
+save_coefficients(mtx, dist, dirpath)
