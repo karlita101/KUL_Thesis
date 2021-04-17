685f6cd07c605e82c303697b32f6fcbb89d98ead
karlita101 <71699120+karlita101@users.noreply.github.com>
2021-04-18 00:20:02 +0200

    Implemented IntelReal sense's opencv_pointcloud_viewer.py
    Works!
    
    Faster frame rate (closer to 20fps) due to the decimation.
    
    FRAMES ARE NOT ALIGNED!

--- get_realsense_parameters.py
+++ get_realsense_parameters.py
@@ -19,6 +19,7 @@ config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
+
 # Start streaming
 pipe_profile=pipeline.start(config)
 
